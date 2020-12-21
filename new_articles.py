e7b35af7afb2dfde3386929da701dedb11369d7c
Larisa Shigareva <thelarisa81@gmail.com>
2020-12-21 15:24:43 +0300

    Added a possibility to check which articles are new

--- new_articles.py
+++ new_articles.py
@@ -12,6 +12,12 @@ def monitor(url):
         print("\nChecking feed...")
         feed = feedparser.parse(url)
 
+        articles_from_txt_file: list = []
+
+        with open("new_articles.txt", "r", encoding="cp1251") as file:
+            for line in file:
+                articles_from_txt_file.append(line.strip())
+
         for entry in feed.entries[:10]:
             if "python" in entry.title.lower():
                 if 'podcast' not in entry.title.lower():
@@ -22,6 +28,12 @@ def monitor(url):
                             else entry.title
                         )
                         print("Match found:", truncated_title)
+
+                        if truncated_title not in articles_from_txt_file:
+                            truncated_title = truncated_title + '\n'
+                            with open("new_articles.txt", "a", 
+                                encoding="cp1251") as writer:
+                                writer.write(truncated_title)
 
         time.sleep(60)
 
